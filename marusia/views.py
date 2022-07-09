90107ade021f3c37b5cfd114cd811d6ba6d47b60
fox-sys <borisberestov@ya.ru>
2022-07-09 13:57:50 +0300

    добавить игру бота в очко

--- marusia/views.py
+++ marusia/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from random import randint
+from random import randint, choice
 
 
 class MarusiaRouter(APIView):
@@ -17,6 +17,22 @@ class MarusiaRouter(APIView):
         10: "10",
         11: "Туз"
     }
+    eatable = [
+        'Яблоко',
+        'Груша',
+        'Пончик',
+        'Помидор',
+        'Белый гриб',
+    ]
+    uneatable = [
+        'Арматура',
+        'Сверлильный станок',
+        'Компьютер',
+        'Волчья ягода',
+        'Мухомор',
+        'Телефон',
+        'Пианино',
+    ]
 
     def post(self, request):
         data = request.data
@@ -45,33 +61,61 @@ class MarusiaRouter(APIView):
     def router(self, data):
         if 'cards_sum' in self.state:
             return self.twenty_one_in_progress(data['request']['command'], self.state['cards_sum'], self.state['cards_count'])
+        if 'eat_count' in self.state:
+            return self.eat_in_progress(data['request']['command'], self.state['eat_count'])
         return self.game_menu(data['request']['command'])
 
     def game_menu(self, command: str):
         if command.lower() == 'двадцать один':
             return self.twenty_one_start()
+        if command.lower() == 'съедобное не съедобное':
+            return self.eat_start()
+        return self.get_help_response()
 
-    def twenty_one_start(self):
-        cards = [self.get_random_card() for i in range(2)]
+    def eat_start(self):
         self.response['response'] = {
-            'text': f'Вы вытянули 2 карты {self.card_decode(cards[0])} и {self.card_decode(cards[1])}',
+            'text': f'Вы зашли в игру съедобное/не съедобное',
             'end_session': False,
         }
-        self.response['session_state']['cards_sum'] = sum(cards)
-        self.response['session_state']['cards_count'] = 2
-        if sum(cards) == '21':
-            self.response['response']['text'] = 'Вы выйграли у вас туз и 10'
-            self.response['response']['end_session'] = True
+        self.response['session_state']['eat_count'] = 0
+        self.response['session_state']['food'] = choice(self.eatable) if randint(0, 1) else choice(self.uneatable)
         return self.response
 
-    def twenty_one_in_progress(self, command, last_sum, last_count):
+    def eat_in_progress(self, command, last_count):
+        pass
+
+    def get_help_response(self):
+        self.response['response'] = {
+            'text': 'Команды:\n Двадцать один \n Съедобное не съедобное \n',
+            'end_session': True
+        }
+        return self.response
+
+    def twenty_one_start(self):
+        user_cards = [self.get_random_card() for i in range(2)]
+        bot_cards = [self.get_random_card() for i in range(randint(2, 4))]
+        self.response['response'] = {
+            'text': f'Вы вытянули 2 карты {self.card_decode(user_cards[0])} и {self.card_decode(user_cards[1])}',
+            'end_session': False,
+        }
+        self.response['session_state']['user_cards_sum'] = sum(user_cards)
+        self.response['session_state']['bot_cards_sum'] = sum(bot_cards)
+        return self.response
+
+    def twenty_one_in_progress(self, command, last_sum, bot_sum):
         if command.lower() == 'ещё':
             card = self.get_random_card()
             if card + last_sum > 21:
-                self.response['response'] = {
-                    'text': f'Вы вытянули {self.card_decode(card)}, вы проиграли',
-                    'end_session': True,
-                }
+                if bot_sum > card + last_sum:
+                    self.response['response'] = {
+                        'text': f'Вы вытянули {self.card_decode(card)}, вы набрали {last_sum + card} очков, у бота {bot_sum}, вы побелили',
+                        'end_session': True,
+                    }
+                else:
+                    self.response['response'] = {
+                        'text': f'Вы вытянули {self.card_decode(card)}, вы проиграли',
+                        'end_session': True,
+                    }
             elif card + last_sum == 21:
                 self.response['response'] = {
                     'text': f'Вы набрали 21 очко, вы победили, урааа',
@@ -83,10 +127,19 @@ class MarusiaRouter(APIView):
                     'end_session': False,
                 }
                 self.response['session_state']['cards_sum'] = card + last_sum
-                self.response['session_state']['cards_count'] = 1 + last_count
-        else:
+                self.response['session_state']['cards_count'] = bot_sum
+        elif 'всё' == command.lower():
             self.response['response'] = {
                 'text': f'Вы набрали {last_sum} очков',
                 'end_session': True,
+            }
+            if bot_sum > last_sum:
+                self.response['response']['text'] += 'Вы проиграли'
+            else:
+                self.response['response']['text'] += 'Вы выйграли'
+        else:
+            self.response['response'] = {
+                'text': f'Напишите либо ещё, либо всё',
+                'end_session': False,
             }
         return self.response
